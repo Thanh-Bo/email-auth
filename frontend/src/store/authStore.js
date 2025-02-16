f0da10954cacd6f58412f731b413ea2018913e94
Thanh-Bo <xuanpq359@gmail.com>
2025-02-16 10:11:44 +0700

    URL updated in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,8 @@
 import {create} from 'zustand';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set) => ({
     user : null , 
